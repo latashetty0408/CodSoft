78650fc1872b9b05cdd46f951d4a3073162a303d
latashetty0408 <2001shettylata@gmail.com>
2023-09-06 20:36:54 +0530

    task_3 completed

---
+++ Task_3_Calculator/Calculator.js
@@ -0,0 +1,27 @@
+let Input_Display = document.querySelector('input');
+let Action_buttons = document.querySelectorAll('.btn');
+let clear = document.getElementById('clear');
+let output = '';
+
+Array.from(Action_buttons).forEach((buttons)=>{
+    buttons.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            output = eval(output);
+            Input_Display.value = output;
+        }
+        else if(e.target.innerHTML == '←'){
+            output = output.toString().slice(0, -1);
+            Input_Display.value=output;
+        }
+        else{
+            console.log(e.target);
+            output += e.target.innerHTML;
+            Input_Display.value = output;
+        }
+    })
+})
+
+clear.addEventListener("click", function() {
+    output = "";
+    Input_Display.value = output
+})
